fc50552f10d18a555139a8dd5241827b47fa3fc3
Uzziel <uzzielcervantes@gmail.com>
2016-03-21 23:11:12 -0600

    Commit initial

---
+++ js/consts.js
@@ -0,0 +1,25 @@
+consts = {
+  monthNames: [
+  'Enero',
+  'Febrero',
+  'Marzo',
+  'Abril',
+  'Mayo',
+  'Junio',
+  'Julio',
+  'Agosto',
+  'Septiembre',
+  'Octubre',
+  'Noviembre',
+  'Diciembre'
+],
+  dayNames: [
+  'Domingo',
+  'Lunes',
+  'Martes',
+  'Miercoles',
+  'Jueves',
+  'Viernes',
+  'Sabado'
+]
+};
