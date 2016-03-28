4e9a503d70a52eed3c1441f8f555ea30fa122d68
Uzziel <uzzielcervantes@gmail.com>
2016-03-27 23:50:47 -0600

    Se agrega segundo cal heatMap  para detalle ( aun sin funcionar)

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 moment.locale("es");
-var year = 2016
+var year = new Date().getFullYear();
+var month = new Date().getMonth();
 var numMonth = 3;
 
 var myDataRef = new Firebase("https://dailyhead.firebaseio.com/");
-//var dt = new Date();
 var data = {};
 
 
 var cal = new CalHeatMap();
+var cal1 = new CalHeatMap();
 
 cal.init({
   domain: "month",
@@ -27,8 +28,7 @@ cal.init({
     position: 'top'
   },
   tooltip: true,
-  start: new Date(),
-  highlight: ['now', data],
+  highlight: ['now'],
   animationDuration: 1500,
   scale: [1,2,3,4,5],
   data: data,
@@ -45,25 +45,57 @@ cal.init({
   },
   subDomainDateFormat: function (date) {
     return moment(date).format("LL");
-  }
-  });
+  },
+  maxDate:new Date(year, month)
+ });
 
   $("#jumpToday").on('click', function (event) {
     cal.jumpTo(new Date(), true);
   });
 
+cal1.init({
+    itemSelector: "#cal-heatmap2",
+    data: data,
+    domain: "day",
+    subDomain: "x_hour",
+    subDomainTextFormat: function (date, value) {
+        return new Date(date).getHours();
+    },
+    range: 1,
+    cellSize: 25,
+    displayLegend: false,
+    cellPadding: 5,
+    tooltip: true,
+    animationDuration: 1500,
+    legend: [2, 4, 6, 8],
+    legendColors: ["#510d63", "#f6fcfd"],
+    cellRadius: 10,
+    domainGutter: 12,
+    domainMargin: 12,
+    label: {
+		position: "left",
+		offset: {
+			x: 20,
+			y: 12
+	    },
+	    width: 110
+    },
+    verticalOrientation: true,
+    domainDynamicDimension: false
+})
 
 myDataRef.on("value", function(snapshot) {
   snapshot.forEach(function (argument) {
     getDatos(argument.val().date, argument.val().value);
   });
   cal.update(data);
+  cal1.update(data);
 });
 
 
 function getDatos(date, value) {
+    console.log(date);
   data[date] = value;
-
 }
 
 function registerEvent(date, count) {
@@ -73,3 +105,10 @@ function registerEvent(date, count) {
                   value: newCount
                   });
 }
+
+$("#domainDynamicDimension-next").on("click", function (e) {
+    e.preventDefault();
+    if (!cal.next()) {
+        alert("No hay mas datos para ver");
+    }
+})
